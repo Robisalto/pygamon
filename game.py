b3acdb98aa8de772f0bbf8e38a073170b0437226
Robin Couturier <robicouturier@gmail.com>
2021-07-12 20:45:39 +0200

    mise en place de la carte sur l'animation

--- game.py
+++ game.py
@@ -6,9 +6,17 @@ class Game:
 
     def __init__(self):
         #creer la fenetre du jeu
-        pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Pygamon - Aventure")
 
+        #charger la carte
+        tmx_data = pytmx.util_pygame.load_pygame("carte.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 2
+
+        #dessiner le groupe de calque
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer,default_layer = 1)
 
     def run(self):
 
@@ -16,6 +24,10 @@ class Game:
         running = True
 
         while running:
+
+            self.group.draw(self.screen)
+            pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
