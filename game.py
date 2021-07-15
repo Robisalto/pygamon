0b8bf3e6622799a9cf4f6e883381dbbe3708943f
Robin Couturier <robicouturier@gmail.com>
2021-07-15 18:58:34 +0200

    mise en place de la maison et de l'entrée sortie

--- game.py
+++ game.py
@@ -27,10 +27,16 @@ class Game:
             if object.type == "collision":
                 self.walls.append(pygame.Rect(object.x, object.y, object.width, object.height))
 
+        # definir le rectangle de collision pour entrer dans la maison
+        enter_house = tmx_data.get_object_by_name('enter_house')
+        self.enter_house_rect = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
 
         #dessiner le groupe de calque, le calque le plus en amont est la calque 3
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer,default_layer = 5)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer = 8)
         self.group.add(self.player)
+
+        #savoir dans quel monde on est
+        self.map = "world"
 
     def handle_input(self):
         pressed = pygame.key.get_pressed()
@@ -47,8 +53,74 @@ class Game:
             self.player.move_left()
             self.player.change_animation("left")
 
+    def switch_house(self):
+        #charger la carte
+        tmx_data = pytmx.util_pygame.load_pygame("house.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 2
+
+
+        #definir une liste qui va stocker les rectangles de collisons
+        self.walls =[]
+
+        for object in tmx_data.objects:
+            if object.type == "collision":
+                self.walls.append(pygame.Rect(object.x, object.y, object.width, object.height))
+
+
+        #dessiner le groupe de calque, le calque le plus en amont est la calque 3
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer,default_layer = 5)
+        self.group.add(self.player)
+
+        #definir le rectangle de collision pour sortir de la maison
+        enter_house = tmx_data.get_object_by_name("exit_house")
+        self.enter_house_rect = pygame.Rect(enter_house.x,enter_house.y,enter_house.width, enter_house.height)
+
+        #definir le point de spawn dans la maison
+        spawn_house_point = tmx_data.get_object_by_name("spawn_house")
+        self.player.position[0]=spawn_house_point.x
+        self.player.position[1]=spawn_house_point.y - 20
+
+    def switch_world(self):
+        # charger la carte
+        tmx_data = pytmx.util_pygame.load_pygame("carte.tmx")
+        map_data = pyscroll.data.TiledMapData(tmx_data)
+        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
+        map_layer.zoom = 2
+
+        # definir une liste qui va stocker les rectangles de collisons
+        self.walls = []
+
+        for object in tmx_data.objects:
+            if object.type == "collision":
+                self.walls.append(pygame.Rect(object.x, object.y, object.width, object.height))
+
+        # dessiner le groupe de calque, le calque le plus en amont est la calque 3
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=6)
+        self.group.add(self.player)
+
+        # definir le rectangle de collision pour entrer dans la maison
+        enter_house = tmx_data.get_object_by_name("enter_house")
+        self.enter_house_rect = pygame.Rect(enter_house.x, enter_house.y, enter_house.width, enter_house.height)
+
+        # definir le point de spawn dans la maison
+        spawn_house_point = tmx_data.get_object_by_name("spawn_house_exit")
+        self.player.position[0] = spawn_house_point.x
+        self.player.position[1] = spawn_house_point.y +5
+
     def update(self):
         self.group.update()
+
+        #verifier l'entree dans la maison
+        if self.map == "world" and self.player.feet.colliderect(self.enter_house_rect):
+            self.switch_house()
+            self.map = "house"
+
+     # verifier l'entree dans la maison
+        if self.map == "house" and self.player.feet.colliderect(self.enter_house_rect):
+            self.switch_world()
+            self.map = "world"
 
         #verification collision
         for sprite in self.group.sprites():
